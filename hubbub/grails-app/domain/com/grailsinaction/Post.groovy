dd515e1aaf3192a4ade604db8d8e3ec05a74bfa4
Queen <going.in.limbo@gmail.com>
2016-04-01 14:43:48 +0100

    Update Domain for hubbub and add ,gitignore for qod

---
+++ hubbub/grails-app/domain/com/grailsinaction/Post.groovy
@@ -0,0 +1,11 @@
+package com.grailsinaction
+
+class Post {
+    String content
+    Date dateCreated
+    static belongsTo = [ user : User ]
+
+    static constraints = {
+        content blank: false
+    }
+}
--- hubbub/grails-app/domain/com/grailsinaction/User.groovy
+++ hubbub/grails-app/domain/com/grailsinaction/User.groovy
@@ -1,7 +1,19 @@
 package com.grailsinaction
 
 class User {
+    String loginId
+    String password
+    String homepage
+    Date dateCreated
+    static hasOne = [profile : Profile]
+    static hasMany = [posts : Post]
 
     static constraints = {
+        loginId size:  3..20 , unique: true, nullable: false
+        password size: 6..8, nullable: false, validator: {password, user ->
+            user.loginId != password
+        }
+        homepage url: true, nullable: true
+        profile nullable: true
     }
 }
