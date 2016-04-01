1a12485578858807cf3a39718ba826cb593d7966
Queen <going.in.limbo@gmail.com>
2016-04-01 18:12:39 +0100

    Fix the integration test for the Tag domain class

--- hubbub/src/integration-test/groovy/com/grailsinaction/PostIntegrationTestSpec.groovy
+++ hubbub/src/integration-test/groovy/com/grailsinaction/PostIntegrationTestSpec.groovy
@@ -36,4 +36,26 @@ class PostIntegrationTestSpec extends Specification {
         then: "The posts appear on the retrieved user"
         sortedPostContent == ['First', 'Second', 'Third']
     }
+
+    def "Exercise tagging several posts with various tags"() {
+        given: "A user with a set of tags"
+        def user = new User(loginId: 'joe', password: 'secret')
+        def tagGroovy = new Tag(name: 'groovy')
+        def tagGrails = new Tag(name: 'grails')
+        user.addToTags(tagGroovy)
+        user.addToTags(tagGrails)
+        user.save(failOnError: true)
+        when: "The user tags two fresh posts"
+        def groovyPost = new Post(content: "A groovy post")
+        user.addToPosts(groovyPost)
+        groovyPost.addToTags(tagGroovy)
+        def bothPost = new Post(content: "A groovy and grails post")
+        user.addToPosts(bothPost)
+        bothPost.addToTags(tagGroovy)
+        bothPost.addToTags(tagGrails)
+        then:
+        user.tags*.name.sort() == ['grails', 'groovy']
+        1 == groovyPost.tags.size()
+        2 == bothPost.tags.size()
+    }
 }
